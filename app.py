2843808683f90f6395b17fefa57a339467969c77
KISHORE D <kishorehore256@gmail.com>
2026-07-08 10:36:41 +0530

    v1

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI
-from typing import Dict, Any
 
 from predictor import predict
 
 from schemas import Passenger
 
-app=FastAPI(title="Titanic Survived API",version="1.0")
+app=FastAPI(
+    title="Titanic Survival API",
+    version="1.0"
+)
 
 @app.get("/")
-def home() -> Dict[str, str]:
-    return {"Titanic Prediction API is running"}    
-
-
+def home():
+    return{
+        "message":"Titanic Prediction API is running."
+    }
 @app.post("/predict")
-def predict_survival(passenger:Passenger) -> Dict[str, Any]:
+def predict_survival(passenger:Passenger):
     result=predict(passenger.model_dump())
     return result
+
